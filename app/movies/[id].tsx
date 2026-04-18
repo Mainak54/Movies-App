9eb04187166fb382225d548d50dca41187fa28f1
Mainak54 <mainakdawn2005@gmail.com>
2026-04-18 17:06:48 +0530

    intialize project structure

---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MoveieDetails = () => {
+  return (
+    <View>
+      <Text>MoveieDetails</Text>
+    </View>
+  )
+}
+
+export default MoveieDetails
